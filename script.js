62ce11eb71a7348c435ddd492dd38a0283ffdd5b
Muhammad Bazaid <156231635+bazaid-awan@users.noreply.github.com>
2025-02-13 23:52:57 -0800

    updated

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("This code is from Shahzad Farooq");
 console.log("This code is from  Umar Farooq");
 
 console.log("This code is written by Rao Maaz");
+
+console.log("This code is written by Bazaid Awan");
